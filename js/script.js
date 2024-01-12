a7bfe9796f7d4854b563784dc7461ab141c4eaf7
emilianogastaldo <emiliano0303@gmail.com>
2024-01-12 16:42:17 +0100

    added point 4 and 5 of scaletta

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // Recupero gli oggeti dall'html
 
 const guessNumberElement = document.getElementById('guess-number');
+const resultMessage = document.getElementById('result-message');
 
 //Creo la funzione per generare numeri randomici da 1 a 100
 const getRandomNumber = () => {
@@ -8,22 +9,38 @@ const getRandomNumber = () => {
 }
 
 //Creo la funzione che genera un array di 5 numeri random
-const getNumbers = () => {
+const getNumbers = (number) => {
     const numbers = [];
-    while (numbers.length < 5) {
+    while (numbers.length < number) {
         const number = getRandomNumber();
         if (!numbers.includes(number)) numbers.push(number);
     }
     return numbers;
 }
-const toGuessNumbers = getNumbers();
+
+//Decido quanti numeri bisogna indovinare
+const questNumber = 5
+
+const toGuessNumbers = getNumbers(questNumber);
 let toGuessNumbersText = '';
 for (let i = 0; i < toGuessNumbers.length; i++) {
     toGuessNumbersText += `<span>${toGuessNumbers[i]}</span>`;
 }
+console.log(toGuessNumbers);
 guessNumberElement.innerHTML = toGuessNumbersText;
 
 setTimeout(() => {
     guessNumberElement.classList.add('hidden');
-}, 5000);
+    setTimeout(() => {
+        let message = 'Hai indovitano i seguenti numeri: ';
+        for (let i = 0; i < toGuessNumbers.length; i++) {
+            const playerNumber = parseInt(prompt('Dammi un numero'));
+            if (toGuessNumbers.includes(playerNumber)) message += playerNumber + ' ';
+        }
+        console.log(message);
+
+        resultMessage.innerText = message;
+
+    }, 1000);
+}, 3000);
 
