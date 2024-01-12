09ec0eb1048ed7a668d2d565e4fbe6c95d7b1fed
emilianogastaldo <emiliano0303@gmail.com>
2024-01-12 16:11:20 +0100

    create getNumbers fuction

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
 // Recupero gli oggeti dall'html
 
-const guessNumber = document.getElementById('guess-number');
+const guessNumberText = document.getElementById('guess-number');
 
 //Creo la funzione per generare numeri randomici da 1 a 100
 const getRandomNumber = () => {
     return Math.floor(Math.random() * 100) + 1;
 }
 
+//Creo l'array di 5 numeri random
+const getNumbers = () => {
+    const numbers = [];
+    while (numbers.length < 5) {
+        const number = getRandomNumber();
+        if (!numbers.includes(number)) numbers.push(number);
+    }
+    return numbers;
+}
+const prova = getNumbers();
+console.log(prova)
